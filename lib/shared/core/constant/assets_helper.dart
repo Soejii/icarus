21b6ac913553f91ff489feaca2a69c76f4ca0e40
Soejii <ninkaishou27@gmail.com>
2026-02-12 12:54:42 +0700

    chore: deleting unused images and icons

--- lib/shared/core/constant/assets_helper.dart
+++ lib/shared/core/constant/assets_helper.dart
@@ -2,9 +2,9 @@ class AssetsHelper {
   static String imgProfilePlaceholder = img("img_profile_placeholder.png");
   static String imgAnnouncementPlaceholder =
       img('img_announcement_placeholder.png');
-  static String imgTask = img("img_task.png");
+
   static String imgQuiz = img("img_quiz.png");
-  static String imgUpload = img('img_upload.png');
+  static String imgSubjectPlaceholder = img("img_subject_placeholder.png");
 
   static String imgHomeButtonMapel = img("img_home_mapel.png");
   static String imgHomeButtonDiskusi = img("img_home_diskusi.png");
@@ -15,34 +15,7 @@ class AssetsHelper {
   static String imgHomeButtonKeuangan = img("img_home_keuangan.png");
   static String imgHomeButtonCbt = img("img_home_keuangan.png");
 
-  static String imgSubjectIpa = img("img_subject_kimia.png");
-  static String imgSubjectIps = img("img_subject_ips.png");
-  static String imgSubjectMatematika = img("img_subject_matematika.png");
-  static String imgSubjectIndonesia = img("img_subject_indonesia.png");
-  static String imgSubjectFisika = img("img_subject_fisika.png");
-  static String imgSubjectBiologi = img("img_subject_biologi.png");
-  static String imgSubjectSejarah = img("img_subject_sejarah.png");
-  static String imgSubjectPai = img("img_subject_pai.png");
-  static String imgSubjectKimia = img("img_subject_kimia.png");
-  static String imgSubjectTik = img("img_subject_tik.png");
-  static String imgSubjectSeni = img("img_subject_seni.png");
-  static String imgSubjectKewarganegaraan =
-      img("img_subject_kewarganegaraan.png");
-  static String imgSubjectEkonomi = img("img_subject_ekonomi.png");
-  static String imgSubjectInggris = img("img_subject_inggris.png");
-  static String imgSubjectGeografi = img("img_subject_geografi.png");
-  static String imgSubjectPlaceholder = img("img_subject_placeholder.png");
-  static String imgSubjectModulActive = img("img_subject_modul_activate.png");
-  static String imgSubjectModulInactive = img("img_subject_modul_inactive.png");
-
   static String imgTidakAdaPelajaran = img("img_tidak_ada_pelajaran.png");
-
-  static String imgMediaYoutube = img("img_media_youtube.png");
-  static String imgMediaWord = img("img_media_word.png");
-  static String imgMediaExcel = img("img_media_excel.png");
-  static String imgMediaPdf = img("img_media_pdf.png");
-  static String imgMediaPpt = img("img_media_ppt.png");
-  static String imgMediaVideo = img("img_media_video.png");
 
   static String imgDataNotFound = img('img_data_not_found.png');
   static String imgNetworkError = img('img_no_internet.png');
@@ -50,11 +23,10 @@ class AssetsHelper {
 
   static String imgSuccess = img('img_done.png');
 
-  
   static String imgChatNotFound = img('img_chat_not_found.png');
-  
+
   static String icHome = icon('ic_beranda_home.svg');
-  static String icActivity = icon('ic_activity_home.svg');
+  static String icPerformance = icon('ic_performance_home.svg');
   static String icChat = icon('ic_chat_home.svg');
   static String icProfile = icon('ic_profile_home.svg');
   static String icSaving = icon('ic_saving.svg');
