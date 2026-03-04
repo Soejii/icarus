163183c7d18141700f9b522ff033bf1b61ff8e4c
Soejii <ninkaishou27@gmail.com>
2026-03-04 15:26:31 +0700

    feat: deleting unused shit

--- lib/features/profile/domain/entities/profile_entity.dart
+++ lib/features/profile/domain/entities/profile_entity.dart
@@ -1,51 +1,29 @@
 import 'package:icarus/features/profile/domain/entities/user_entity.dart';
 
 class ProfileEntity {
-  final int userId;
   final String name;
   final String className;
   final String imgUrl;
   final String username;
   final String email;
-  final String nis;
-  final String nisn;
   final String birthplace;
   final String birthdate;
-  final String gender;
   final String religion;
   final String address;
-  final String rt;
-  final String rw;
-  final String dusun;
-  final String kelurahan;
-  final String kecamatan;
-  final String codePos;
-  final String schoolOrigin;
   final String nik;
   final String phone;
   final UserEntity? user;
 
   ProfileEntity({
-    required this.userId,
     required this.name,
     required this.className,
     required this.imgUrl,
     required this.username,
     required this.email,
-    required this.nis,
-    required this.nisn,
     required this.birthplace,
     required this.birthdate,
-    required this.gender,
     required this.religion,
     required this.address,
-    required this.rt,
-    required this.rw,
-    required this.dusun,
-    required this.kelurahan,
-    required this.kecamatan,
-    required this.codePos,
-    required this.schoolOrigin,
     required this.nik,
     required this.phone,
     this.user,
