59b79a538053665632dc67e3ea7c9aad3062ac5c
Soejii <ninkaishou27@gmail.com>
2026-04-06 14:19:43 +0700

    fix: fixed bad url on list child

--- lib/features/child/data/datasource/child_remote_datasource.dart
+++ lib/features/child/data/datasource/child_remote_datasource.dart
@@ -7,7 +7,7 @@ class ChildRemoteDataSource {
   final Dio _dio;
 
   Future<List<ChildModel>> getChildren() async {
-    final res = await _dio.get('/api/parent/list-my-child');
+    final res = await _dio.get('/list-my-child');
     final data = (res.data as Map<String, dynamic>)['data'] as List<dynamic>;
     return data
         .map((e) => ChildModel.fromJson(Map<String, dynamic>.from(e as Map)))
